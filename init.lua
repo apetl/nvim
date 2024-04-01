a878c621106d99a7788ca554bb84a4632c1205bb
Ayan <petlikarayan@gmail.com>
2024-04-01 10:13:00 -0400

    style(init.lua): standardize quotes for consistency in vim.opt.guicursor feat(init.lua): add highlight for CursorLine with no background color for better visibility
    refactor(keymaps.lua): comment out NvimTreeToggle keymap to disable it temporarily

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.opt.guicursor = { 'sm:block,n-v-c:ver25,i-ci-ve:ver25-blinkwait100-blinkon100-blinkoff100,r-cr-o:hor20' }
+vim.opt.guicursor = { "sm:block,n-v-c:ver25,i-ci-ve:ver25-blinkwait100-blinkon100-blinkoff100,r-cr-o:hor20" }
+vim.cmd([[highlight CursorLine ctermbg=NONE guibg=NONE]])
