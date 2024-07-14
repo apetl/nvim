e7128d1cf79479da332afeb7942247823911f4a6
Ayan <petlikarayan@gmail.com>
2024-07-14 16:50:51 -0500

    chore(plugins): comment out folke/noice.nvim plugin instead of disabling it

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
-  { "folke/noice.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   -- { "nvim-tree/nvim-tree.nvim", enabled = false },
