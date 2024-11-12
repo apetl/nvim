fade3241f1a3780dd7f381044aecdc1723d298b1
Ayan <petlikarayan@gmail.com>
2024-11-11 22:30:45 -0500

    feat(lazyvim.json): add support for Go and Markdown languages
    Add Go and Markdown to the list of supported languages to enhance
    language support. Update news and version to reflect changes.
    
    chore(keymaps.lua): comment out custom key mappings
    
    Comment out custom key mappings to prevent conflicts or unintended
    behavior during navigation and window management.
    
    chore(lazy.lua): disable tutor plugin
    
    Disable the tutor plugin to streamline the plugin setup and reduce
    unnecessary loading.
    
    feat(plugins/avante.lua): add Avante plugin configuration
    
    Introduce Avante plugin with specific configuration for enhanced
    functionality. Include dependencies and setup for Windows build
    process.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,26 +12,26 @@ vim.api.nvim_set_keymap(
 )
 
 -- Map new movement keys
-vim.api.nvim_set_keymap("n", "i", "k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "j", "h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "k", "j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "l", "l", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap("v", "i", "k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "j", "h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "k", "j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "l", "l", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap("o", "i", "k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("o", "j", "h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("o", "k", "j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("o", "l", "l", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap("n", ";", "i", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap("n", "<S-j>", ":bprevious<CR>", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap("n", "<C-i>", "<C-w>k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "i", "k", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "j", "h", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "k", "j", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "l", "l", { noremap = true, silent = true })
+--
+-- vim.api.nvim_set_keymap("v", "i", "k", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("v", "j", "h", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("v", "k", "j", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("v", "l", "l", { noremap = true, silent = true })
+--
+-- vim.api.nvim_set_keymap("o", "i", "k", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("o", "j", "h", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("o", "k", "j", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("o", "l", "l", { noremap = true, silent = true })
+--
+-- vim.api.nvim_set_keymap("n", ";", "i", { noremap = true, silent = true })
+--
+-- vim.api.nvim_set_keymap("n", "<S-j>", ":bprevious<CR>", { noremap = true, silent = true })
+--
+-- vim.api.nvim_set_keymap("n", "<C-i>", "<C-w>k", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>h", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>j", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
