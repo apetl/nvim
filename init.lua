e5c56c31d55c32599935b9197938ffeb03375142
apetl <git@apetl.dev>
2025-03-27 22:01:23 -0400

    complete lazyvim reinstallation for clean configuration

--- init.lua
+++ init.lua
@@ -4,8 +4,6 @@ require("config.lazy")
 vim.opt.guicursor = { "sm:block,n-v-c:ver25,i-ci-ve:ver25-blinkwait100-blinkon100-blinkoff100,r-cr-o:hor20" }
 vim.cmd([[highlight CursorLine ctermbg=NONE guibg=NONE]])
 
-vim.o.shell = "pwsh.exe"
-
 if vim.fn.has("wsl") == 1 then
   vim.g.clipboard = {
     name = "win32yank-wsl",
@@ -20,35 +18,3 @@ if vim.fn.has("wsl") == 1 then
     cache_enabled = 1,
   }
 end
-
-require("scrollEOF").setup()
-
--- Override the cmp.entry.get_documentation setting
--- require("cmp").setup({
---   lsp = {
---     override = {
---       ["cmp.entry.get_documentation"] = {
---         enabled = false,
---       },
---     },
---   },
--- })
-
--- Custom highlights for documentation
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e", fg = "#cdd6f4" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#1e1e2e", fg = "#cdd6f4" })
-vim.api.nvim_set_hl(0, "CmpDocumentation", { bg = "#1e1e2e", fg = "#cdd6f4" })
-vim.api.nvim_set_hl(0, "CmpDocumentationBorder", { bg = "#1e1e2e", fg = "#cdd6f4" })
-
---vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
---vim.api.nvim_create_autocmd("LspAttach", {
---  group = "LspAttach_inlayhints",
---  callback = function(args)
---    if not (args.data and args.data.client_id) then
---      return
---    end
---    local bufnr = args.buf
---    local client = vim.lsp.get_client_by_id(args.data.client_id)
---    require("lsp-inlayhints").on_attach(client, bufnr)
---  end,
---})
--- lua/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- { "folke/noice.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  -- { "nvim-tree/nvim-tree.nvim", enabled = false },
-  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
-}
