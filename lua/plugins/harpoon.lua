e466ac4319ffd8de02f5428c151ccf1b6e4bb9bd
apetl <git@apetl.dev>
2025-03-28 01:56:59 -0400

    updated harpoon to harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,53 +1,65 @@
 return {
   "ThePrimeagen/harpoon",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("harpoon").setup({
-      global_settings = {
-        save_on_toggle = false,
-        save_on_change = true,
-        enter_on_sendcmd = false,
-        tmux_autoclose_windows = false,
-        excluded_filetypes = { "harpoon" },
-        mark_branch = false,
-      },
-    })
-  end,
-  keys = {
-    {
-      "<leader>ha",
-      function()
-        require("harpoon.mark").add_file()
-      end,
-      desc = "Add file to harpoon",
+  branch = "harpoon2",
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
     },
-    {
-      "<leader>hr",
-      function()
-        require("harpoon.mark").rm_file()
-      end,
-      desc = "Remove file from harpoon",
-    },
-    {
-      "<C-e>",
-      function()
-        require("harpoon.ui").toggle_quick_menu()
-      end,
-      desc = "Toggle quick menu",
-    },
-    {
-      "<leader>h]",
-      function()
-        require("harpoon.ui").nav_next()
-      end,
-      desc = "Go to next harpoon mark",
-    },
-    {
-      "<leader>h[",
-      function()
-        require("harpoon.ui").nav_prev()
-      end,
-      desc = "Go to previous harpoon mark",
+    settings = {
+      save_on_toggle = true,
     },
   },
+  keys = function()
+    local harpoon = require("harpoon")
+
+    local keys = {
+      {
+        "<leader>ha",
+        function()
+          harpoon:list():add()
+        end,
+        desc = "Add file to harpoon",
+      },
+      {
+        "<leader>hr",
+        function()
+          harpoon:list():remove()
+        end,
+        desc = "Remove file from harpoon",
+      },
+      {
+        "<C-h>",
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Toggle quick menu",
+      },
+      {
+        "<leader>h]",
+        function()
+          harpoon:list():next()
+        end,
+        desc = "Go to next harpoon mark",
+      },
+      {
+        "<leader>h[",
+        function()
+          harpoon:list():prev()
+        end,
+        desc = "Go to previous harpoon mark",
+      },
+    }
+
+    for i = 1, 5 do
+      table.insert(keys, {
+        "<leader>" .. i,
+        function()
+          harpoon:list():select(i)
+        end,
+        desc = "Harpoon to File " .. i,
+      })
+    end
+
+    return keys
+  end,
 }
