f9045a55ce3b20a3e308715484e1a49bab8476d8
apetl <git@apetl.dev>
2025-06-02 18:33:17 -0500

    remove custom blink keymaps

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -9,25 +9,6 @@ return {
         enabled = false,
       },
     },
-    keymap = {
-      ["<Up>"] = {},
-      ["<Down>"] = {},
-      ["<C-CR>"] = { "select_and_accept" },
-      ["<CR>"] = {},
-      ["<S-space>"] = { "select_and_accept" },
-      ["<Tab>"] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
-        end,
-        "snippet_forward",
-        "fallback",
-      },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-    },
     sources = {
       default = { "avante", "lsp", "path", "snippets", "buffer" },
       providers = {
