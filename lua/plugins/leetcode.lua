3ea040af6da17d99bfcb0cba196c40a1a3625917
apetl <git@apetl.dev>
2025-08-06 17:24:14 -0500

    leetcode plugin

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,10 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html",
+  dependencies = {
+    picker = { provider = nil },
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {},
+}
