b9086d29ec2819cb5d5c459056a5d42f7328d095
apetl <git@apetl.dev>
2025-08-08 18:13:05 -0500

    fix for :LspStart command

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,7 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 require("config.obsidian")
+
+--Tempory fix for :LspStart command
+vim.api.nvim_create_augroup("nvim.lsp.enable", { clear = false })
+vim.cmd.doautoall("nvim.lsp.enable FileType")
