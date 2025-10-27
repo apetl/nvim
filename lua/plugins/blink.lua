e4e13f182f68dc76c6fcc8779c20f8fb306d868a
apetl <git@apetl.dev>
2025-10-26 19:59:37 -0500

    disable suggestions on .md files

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,6 +4,9 @@ return {
     "Kaiser-Yang/blink-cmp-avante",
   },
   opts = {
+    enabled = function()
+      return vim.bo.filetype ~= "markdown"
+    end,
     completion = {
       ghost_text = {
         enabled = false,
